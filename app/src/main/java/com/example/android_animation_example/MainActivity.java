da0d63cbaabfe9bd460e0a2c2405b6f3bea3993b
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-05-09 20:22:10 +0530

    add a simple animation

--- app/src/main/java/com/example/android_animation_example/MainActivity.java
+++ app/src/main/java/com/example/android_animation_example/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android_animation_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void imageViewIsTapped (View imageView) {
+
+        ImageView tappedImageView = (ImageView) imageView;
+
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.tiger);
+        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+    }
+
 }
