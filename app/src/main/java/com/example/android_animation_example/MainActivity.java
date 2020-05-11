440c837584cebf6a58ed925eab540d8de4876767
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-05-11 13:54:48 +0530

    add a player enum

--- app/src/main/java/com/example/android_animation_example/MainActivity.java
+++ app/src/main/java/com/example/android_animation_example/MainActivity.java
@@ -8,6 +8,14 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    enum Player {
+
+        ONE, TWO
+
+    }
+
+    Player currentPlayer = Player.ONE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
         ImageView tappedImageView = (ImageView) imageView;
 
         tappedImageView.setTranslationX(-2000);
-        tappedImageView.setImageResource(R.drawable.tiger);
+
+        if (currentPlayer == Player.ONE) {
+
+            tappedImageView.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.TWO;
+
+        } else if (currentPlayer == Player.TWO) {
+
+            tappedImageView.setImageResource(R.drawable.lion);
+            currentPlayer = Player.ONE;
+
+        }
         tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
 
     }
